8b20b576061f7c8507d3b3724e689264787b290c
Jung Wonseok <ws.jung@anypointmedia.com>
2021-05-11 19:02:19 +0900

    initialized project

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "1.4.32"
+    groovy
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.codehaus.groovy:groovy:3.0.7")
+    testImplementation(platform("org.spockframework:spock-bom:2.0-M4-groovy-3.0"))
+    testImplementation("org.spockframework:spock-core")
+
+    // logging
+    implementation("org.slf4j:slf4j-api:1.7.30")
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+    implementation("io.github.microutils:kotlin-logging:2.0.3")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
