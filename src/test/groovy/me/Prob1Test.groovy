204a0e9d30a2c8b4fb1f942ed72762c8d837b605
Ginny <d@ad.com>
2021-05-17 20:26:21 +0900

    final commit for Problem1

--- src/test/groovy/me/Prob1Test.groovy
+++ src/test/groovy/me/Prob1Test.groovy
@@ -3,26 +3,22 @@ package me
 import spock.lang.Specification
 
 class Prob1Test extends Specification {
-    def "Prob1"(){
-//        given:
-//        def a = "2+3"
-//        def expected = 5
-
+    def Calculator(){
         when:
-        def actual = new Prob1().calculator(a)
+        def actual = new Calculator().calculatingString(a)
 
         then:
         actual == expected
 
-        //디버그
         where:
         a | expected
-//        "1+1" | 2
-//        "2*3" | 6
-//        "3/2" | 1.5
-//        "200*300" | 200*300
-        " 2+3" | 5
-//        "2//3" | null
+
+        "1+1" | 2
+        "2*3" | 6
+        "3/2" | 1.5
+        "200*300" | 200*300
+        "2//3" | null
+        "2++3" | null
 
     }
 
