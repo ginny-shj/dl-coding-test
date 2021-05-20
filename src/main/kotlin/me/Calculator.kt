ccef740a1afa20ed255dc84ed15f6299594c9d38
Ginny <d@ad.com>
2021-05-20 13:57:42 +0900

    problem1

---
+++ src/main/kotlin/me/Calculator.kt
@@ -0,0 +1,45 @@
+package me
+
+import java.lang.Exception
+import java.math.BigDecimal
+
+
+class Calculator {
+    fun calculatingString(data: String): Number? {
+
+        val regex = """(\d+)(\+*|\-*|\**|\/*)(\d+)""".toRegex()
+        val matchResult = regex.find(data)
+
+        try {
+            val (first, op, second) = matchResult!!.destructured
+
+            return when (op) {
+                "+" -> first.toBigDecimal() + second.toBigDecimal()
+                "-" -> first.toBigDecimal() - second.toBigDecimal()
+                "*" -> first.toBigDecimal() * second.toBigDecimal()
+                "/" -> first.toBigDecimal().divide(second.toBigDecimal(), 4, BigDecimal.ROUND_DOWN)
+                else -> {
+                    println("+, -, *, / 연산자를 입력해주세요.")
+                    null
+                }
+            }
+        } catch(e: NullPointerException){
+            println("식을 바르게 입력해 주세요")
+            return null
+        }
+        catch( e: ArithmeticException) {
+            println("${Double.MIN_VALUE} 보다 크게, ${Double.MAX_VALUE} 보다 작은 값을 넣어주세요")
+            return null
+        } catch (e: Exception) {
+            println("입력값이 잘못 되었습니다.")
+            return null
+        }
+    }
+}
+
+fun main(args: Array<String>){
+    val inputData = readLine().toString().replace(" ", "")
+    val cal = Calculator()
+    println(cal.calculatingString(inputData))
+
+}
